933b93ffb17ecd9e2b204db7b359f7c1b7014217
Alvin Julian <images2907@yahoo.co.id>
2016-05-05 12:01:36 +0700

    EntityRepo Nambahin Interface di EntityRepository, sebagai kerangka yang nantinya bisa di custom di tiap class

---
+++ NorthwindRepository/Interfaces/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindRepository.Interfaces
+{
+    interface IEntityRepository<T, P>
+    {
+        IQueryable<T> GetAllData();
+
+        T Search(P id);
+
+        void Insert(T entity);
+
+        void Delete(P id);
+
+        void Update(T entity);
+    }
+}
