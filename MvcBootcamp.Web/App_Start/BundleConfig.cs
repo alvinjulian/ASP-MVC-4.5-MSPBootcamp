bf1b61d91f282bf992c66a19adabf291b6b1e549
Alvin Julian <images2907@yahoo.co.id>
2016-05-05 10:14:58 +0700

    PartialView & Style Bundle Config Bundle config untuk nambahin CSS dan JS Pemanggilannya pakai StyleRender
    PartialView untuk memanggil HTML sebagian.
    render mengembalikan string, renderpartial sama seperti void

--- MvcBootcamp.Web/App_Start/BundleConfig.cs
+++ MvcBootcamp.Web/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace MvcBootcamp.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/mycss").Include(
+                      "~/Content/myStyle.css"));
+
         }
     }
 }
