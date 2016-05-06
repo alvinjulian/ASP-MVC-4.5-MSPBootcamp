af33b547c5f94d0d417ea8022e0c43b2b0e27943
Alvin Julian <images2907@yahoo.co.id>
2016-05-06 13:22:05 +0700

    AttributeRoutes Menggunakan AttributeRoute untuk setiap controller. RoutePrefis menjadikan template untuk Route yang ada di bagian bawahnya. Untuk enable attribute route, harus menambahkan routes.MapMvcAttributeRoutes(); pada RouteConfig. Traditional Route dilakukan di RouteCOnfig Routing menggunakan { } berarti value yang ada di dalam kurung kurawal sudah predefined (seperti id, nama dsb)

--- MvcBootcamp.Web/Controllers/CustomerRouteController.cs
+++ MvcBootcamp.Web/Controllers/CustomerRouteController.cs
@@ -7,16 +7,21 @@ using NorthwindRepository.Repositories;
 
 namespace MvcBootcamp.Web.Controllers
 {
+    [RoutePrefix("CustomerData")]
     public class CustomerRouteController : Controller
     {
         private CustomerRepository custRepo = new CustomerRepository();
-
         // GET: CustomerAJAX
+
+        //[Route("CustomerData/AllCustomer")]
+        [Route("AllCustomer")] // kalau pakai prefix
         public ActionResult Index()
         {
             return View(custRepo.GetAllData().Take(5));
         }
 
+        //[Route("CustomerData/{id}")]
+        [Route("{id}")] //kalau pakai prefix
         public ActionResult Search(string id)
         {
             return View(custRepo.Search(id));
