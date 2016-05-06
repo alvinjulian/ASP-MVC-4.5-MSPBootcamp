18eafb786b5aadd56650b229768e13dea6e673d1
Alvin Julian <images2907@yahoo.co.id>
2016-05-06 10:59:27 +0700

    Mengubah RouteConfig Mengubah routing yang aslinya URL asli menggunakan tag lainnya, sekarang menggunakan / sebagai pembatas setiap url. URL juga dibuat lebih human readable, SEO Friendly

--- MvcBootcamp.Web/App_Start/RouteConfig.cs
+++ MvcBootcamp.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MvcBootcamp.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Store",
+                url: "MyStore/BrowseProduct/{category}",
+                defaults: new { controller = "Store", action = "Browse", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
