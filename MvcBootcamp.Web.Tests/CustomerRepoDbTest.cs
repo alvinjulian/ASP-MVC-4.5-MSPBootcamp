b17325f9a41b29f98b7352965fc953bb997bc1c4
Alvin Julian <images2907@yahoo.co.id>
2016-05-08 10:27:55 +0700

    Unit Testing without Framework Membuat unit testing tanpa menggunakan mocking framework

---
+++ MvcBootcamp.Web.Tests/CustomerRepoDbTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DataAccessLayer;
+using NorthwindRepository.Repositories;
+using System.Linq;
+
+namespace MvcBootcamp.Web.Tests
+{
+    [TestClass]
+    public class CustomerRepoDbTest
+    {
+        [TestMethod]
+        public void GetAllData_ReturnAllCustomer()
+        {
+            var repo = new CustomerRepository();
+            var allCust = repo.GetAllData().ToList();
+            Assert.AreEqual(92, allCust.Count());
+        }
+
+        [TestMethod]
+        public void Search_ReturnACustomer()
+        {
+            var repo = new CustomerRepository();
+            var cust = repo.Search("ALFKI");
+            Assert.AreNotEqual("Alfred F", cust.CompanyName);
+        }
+    }
+}
